298d89268d4ec54186fdefc58e21623fed212923
robo <palr74502@gmail.com>
2024-04-20 00:01:10 +0530

    ladkfj

--- server/index.js
+++ server/index.js
@@ -20,12 +20,25 @@ const app = express();
 
 // here i specify that we accept connection from only localhost 3000
 // => if we use like this ==> app.use(cors("*")), it means we accept connection from anywhere 
+
 app.use(cors({
     origin: ["http://localhost:3000", "http://localhost:3001", "https://swift-tasker.vercel.app", "https://swift-tasker-hrchb1dpe-pabitra-pals-projects.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }))
-// app.use(cors("*"))
+
+// option 2 .......
+// const corsOptions = {
+//     origin: ["http://localhost:3000", "http://localhost:3001", "https://swift-tasker.vercel.app", "https://swift-tasker-hrchb1dpe-pabitra-pals-projects.vercel.app"],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+// };
+
+// app.use("/api", cors(corsOptions), router);
+
+app.get("/", (req, res) => {
+    res.redirect("/api"); // Redirect to API documentation
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -34,15 +47,13 @@ app.use(cookieParser());
 
 app.use(morgan("dev"));
 
-app.use("/api", router);
+app.use("/api", router)
 
 // this below middleware is use, when you hit an endpoint url that does not exist , bleow is handle our all error
 app.use(routeNotFound)
 app.use(errorHandler)
 
-app.get("/", (req, res) => {
-    res.send("Welcome to my backend service!");
-});
+
 
 app.listen(PORT, () => {
     console.log(`Server starting on ${PORT}`);
