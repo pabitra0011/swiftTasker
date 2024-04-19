b7e35dbfdb6d67eb2e5d9215e15ae4c72b1a47ea
robo <palr74502@gmail.com>
2024-04-20 00:20:02 +0530

    fix routing issue

--- server/index.js
+++ server/index.js
@@ -36,9 +36,9 @@ app.use(cors({
 
 // app.use("/api", cors(corsOptions), router);
 
-app.get("/", (req, res) => {
-    res.redirect("/api"); // Redirect to API documentation
-});
+// app.get("/", (req, res) => {
+//     res.redirect("/api"); // Redirect to API documentation
+// });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -47,7 +47,7 @@ app.use(cookieParser());
 
 app.use(morgan("dev"));
 
-app.use("/api", router)
+app.use("/", router)
 
 // this below middleware is use, when you hit an endpoint url that does not exist , bleow is handle our all error
 app.use(routeNotFound)
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ import taskRoutes from './taskRoutes.js'
 
 const router = express.Router();
 
-router.use("/user", userRoutes)  //like:  api/user/login
-router.use("/task", taskRoutes)
+router.use("/api/user", userRoutes)  //like:  api/user/login
+router.use("/api/task", taskRoutes)
 
 export default router;
