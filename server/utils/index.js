b46d559c63c4a8cb72e900b411de19a5ac519480
robo <palr74502@gmail.com>
2024-04-20 14:00:19 +0530

    dlfj

--- server/utils/index.js
+++ server/utils/index.js
@@ -17,7 +17,7 @@ export const createJWT = async (res, userId) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "30d" });
     // { expiresIn: "1d" }
     res.cookie("token", token, {
-        httpOnly: true,
+        httpOnly: false,
         secure: process.env.NODE_ENV === "production",
         sameSite: "none", // prevent CSRF attacks
         maxAge: 30 * 24 * 60 * 60 * 1000, // 1day
