59224dfbd7a53f076926309389b381c1e7ab17bb
robo <palr74502@gmail.com>
2024-04-21 20:23:13 +0530

    3

--- server/utils/index.js
+++ server/utils/index.js
@@ -15,17 +15,24 @@ export const dbConnection = async () => {
 
 export const createJWT = async (res, userId) => {
     try {
+        const isProduction = process.env.NODE_ENV === 'production';
+
         const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "30d" });
-        console.log("Token:", token);
+
+        console.log("token:", token);
+
         res.cookie("token", token, {
             httpOnly: true,
-            secure: true,
+            secure: isProduction,
             sameSite: "strict",
-            maxAge: 30 * 24 * 60 * 60 * 1000,
-            path: "/",
+
         });
 
     } catch (error) {
         console.error("Error setting cookie:", error);
     }
 };
+
+
+// maxAge: 30 * 24 * 60 * 60 * 1000,
+// path: "/",
